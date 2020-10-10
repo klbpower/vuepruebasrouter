361298f91407622b6c3d6f6a41478f6b8e344a2f
Sergio C <sergiocalvosos@gmail.com>
2020-10-10 15:21:35 +0200

    Consumir API JSON Placeholder boton

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,9 @@
 <template>
   <Titulo texto="Título de mi blog" />
+  <button @click="consumirAPI">Consumir API JSON Placeholder</button>
+  <div v-for="item in arrayBlog" :key="item.id">
+    {{ item.title }}
+  </div>
 </template>
 
 <script>
@@ -7,6 +11,23 @@ import Titulo from '../components/Titulo'
 export default {
   components:{
     Titulo
+  },
+  data() {
+    return {
+      arrayBlog: []
+    }
+  },
+  methods:{
+   async consumirAPI(){
+      try{
+        const data  = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const array = await data.json();
+        console.log(array);
+        this.arrayBlog = array;
+      }catch(error){
+        console.log(error);
+      }
+    }
   }
 }
 </script>
